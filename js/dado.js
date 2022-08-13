045afb00165b04680a37e81622223581cd2a5ecb
Zacoff <daniel.dfa81@gmail.com>
2022-08-13 10:38:04 -0300

    1310h36AGO - Refactoring

--- js/dado.js
+++ js/dado.js
@@ -1,24 +1,6 @@
 function aperte(){
-var sorteio = Math.floor(Math.random() * 6 + 1);
+    let sorteio = Math.floor(Math.random() * 6 + 1);
     
-switch (sorteio) {
-    case 1:
-        document.getElementById('face').src = './image/um.png';
-        break;
-    case 2:
-        document.getElementById('face').src = './image/dois.png'
-        break;
-    case 3:
-        document.getElementById('face').src = './image/tres.png'
-        break;
-    case 4:
-        document.getElementById('face').src = './image/quatro.png'
-        break;
-    case 5:
-        document.getElementById('face').src = './image/cinco.png'
-        break;
-    case 6:
-        document.getElementById('face').src = './image/seis.png'
-        break;
-    default:
-}};
+    document.getElementById('face').src = `./image/${sorteio}.png`;
+        
+};
